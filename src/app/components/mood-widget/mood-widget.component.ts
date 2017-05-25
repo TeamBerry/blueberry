5142de1144ea1f4e371e8f40d191274578587e5e
AngelZatch <pinbouen.andreas@gmail.com>
2017-05-25 19:47:51 +0200

    Like/Unlike video
    - TODO: Refresh ID in mood widget when the video changes

--- src/app/components/mood-widget/mood-widget.component.ts
+++ src/app/components/mood-widget/mood-widget.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MoodService } from 'app/services/mood.service';
 
 @Component({
-  selector: 'app-mood-widget',
-  templateUrl: './mood-widget.component.html',
-  styleUrls: ['./mood-widget.component.scss']
+    selector: 'app-mood-widget',
+    templateUrl: './mood-widget.component.html',
+    styleUrls: ['./mood-widget.component.scss'],
+    providers: [MoodService]
 })
 export class MoodWidgetComponent implements OnInit {
+    @Input() currentVideo: number;
+    currentVote = null;
 
-  constructor() { }
+    constructor(
+        public moodService: MoodService
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.checkVote();
+    }
+
+    checkVote() {
+        const vote = {
+            user_token: 'D1JU70',
+            video_index: this.currentVideo
+        };
+        this.moodService.checkVote(vote).subscribe(
+            response => {
+                this.currentVote = response;
+            }
+        );
+    }
+
+    addVote() {
+        const vote = {
+            vote_mood: 1,
+            user_token: 'D1JU70',
+            video_index: this.currentVideo
+        };
+        this.moodService.likeVideo(vote).subscribe(
+            response => {
+                vote['vote_id'] = response;
+                this.currentVote = vote;
+            }
+        );
+    }
+
+    removeVote(id: number) {
+        this.moodService.unlikeVideo(id).subscribe(
+            response => {
+                this.currentVote = null;
+            }
+        );
+    }
+
+    requestSkip() {
+
+    }
 
 }
