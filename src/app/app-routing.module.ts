f364df186bd2ca983aa968bacae6874d7d946384
Andreas Pinbouen <pinbouen.andreas@gmail.com>
2017-05-29 13:59:47 +0200

    WiP box form component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from 'app/components/home/home.component';
+import { BoxFormComponent } from 'app/components/box-form/box-form.component';
 
 const appRoutes: Routes = [
-    {path: 'home', component: HomeComponent}
+    { path: 'home', component: HomeComponent },
+    { path: 'create', component: BoxFormComponent },
 ];
 
 @NgModule({
--- src/app/components/box-form/box-form.component.ts
+++ src/app/components/box-form/box-form.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-box-form',
-  templateUrl: './box-form.component.html',
-  styleUrls: ['./box-form.component.scss']
+    selector: 'app-box-form',
+    templateUrl: './box-form.component.html',
+    styleUrls: ['./box-form.component.scss']
 })
 export class BoxFormComponent implements OnInit {
+    boxForm: FormGroup;
+    langs: string[] = [
+        'English',
+        'Français',
+        '日本語',
+    ];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.boxForm = new FormGroup({
+            title: new FormControl(),
+            language: new FormControl()
+        });
+    }
+
+    onSubmit() {
+
+    }
 
 }
