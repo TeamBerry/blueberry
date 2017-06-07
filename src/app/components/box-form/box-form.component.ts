0d28100cad3a4931e1565716aef79cfddc85449b
Andreas Pinbouen <pinbouen.andreas@gmail.com>
2017-06-07 18:28:25 +0200

    Creation and edition of box

--- src/app/components/box-form/box-form.component.ts
+++ src/app/components/box-form/box-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Box } from './../../box';
 import { BoxService } from './../../services/box.service';
 
@@ -8,7 +9,8 @@ import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
     selector: 'app-box-form',
     templateUrl: './box-form.component.html',
-    styleUrls: ['./box-form.component.scss']
+    styleUrls: ['./box-form.component.scss'],
+    providers: [BoxService]
 })
 export class BoxFormComponent implements OnInit {
     @Input() title;
@@ -22,12 +24,27 @@ export class BoxFormComponent implements OnInit {
 
     model = new Box('', '', '', this.langs[0]);
 
-    constructor(public activeModal: NgbActiveModal) { }
+    constructor(
+        public boxService: BoxService,
+        public activeModal: NgbActiveModal,
+        private router: Router) { }
 
     ngOnInit() {
     }
 
     onSubmit() {
         this.submitted = true;
+        if (this.model.token === '') {
+            this.boxService.post(this.model).subscribe(
+                data => {
+                    this.activeModal.close();
+                    this.router.navigate(['/box/' + data.token]);
+                }
+            );
+        } else {
+            this.boxService.put(this.model).subscribe(
+                data => console.log(data)
+            );
+        }
     }
 }
--- src/app/services/box.service.ts
+++ src/app/services/box.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { environment } from './../../environments/environment';
+import { Box } from "app/box";
 
 @Injectable()
 export class BoxService {
@@ -17,6 +18,20 @@ export class BoxService {
 
     get(token: string) {
         return this.http.get(environment.apiUrl + '/boxes/' + token)
+            .map((response: Response) => {
+                return response.json();
+            });
+    }
+
+    post(box: Box) {
+        return this.http.post(environment.apiUrl + '/boxes', box)
+            .map((response: Response) => {
+                return response.json();
+            });
+    }
+
+    put(box: Box) {
+        return this.http.put(environment.apiUrl + '/boxes/' + box.token, box)
             .map((response: Response) => {
                 return response.json();
             });
