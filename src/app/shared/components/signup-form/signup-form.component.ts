c9459c841a8acd78d851837133e5c758c0a0d13c
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2018-06-21 21:03:36 +0200

    Form conversion
    - Verification for password verify

--- src/app/shared/components/signup-form/signup-form.component.ts
+++ src/app/shared/components/signup-form/signup-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from '../../../core/auth/auth.service';
 
 @Component({
     selector: 'app-signup-form',
@@ -7,16 +8,34 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
     styleUrls: ['./signup-form.component.scss']
 })
 export class SignupFormComponent implements OnInit {
+    mail: string;
+    password: string;
+    passwordVerify: string;
+
+    errorMessage: string = null;
 
     constructor(
-        public activeModal: NgbActiveModal
+        public activeModal: NgbActiveModal,
+        public authService: AuthService
     ) { }
 
     ngOnInit() {
     }
 
-    onSubmit(){
+    onSubmit() {
         console.log('attempting to signup...');
+        if (this.password !== this.passwordVerify) {
+            this.errorMessage = 'Your password verification is invalid.';
+        }
+    }
+
+    signup() {
+        this.authService.signup(this.mail, this.password).subscribe(
+            (authResult) => {
+                this.authService.setSession(authResult);
+                location.reload();
+            }
+        )
     }
 
 }
