13adfecadb8671cbe5216ede103747678ef442aa
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2018-08-31 09:24:03 +0200

    Cleaning routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './shared/components/home/home.component';
-import { LikesComponent } from './shared/components/likes/likes.component';
-import { UserSettingsComponent } from './components/user-settings/user-settings.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
