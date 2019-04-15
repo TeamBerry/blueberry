41cc19d662073f6e17390579cf1a6827a9e8a1ff
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2019-04-15 18:48:51 +0200

    Change chronos to athena in subdomains

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  chronosUrl: 'http://chronos.berrybox.tv',
-  hermesUrl: 'http://hermes.berrybox.tv',
+    production: true,
+    chronosUrl: 'http://athena.berrybox.tv',
+    hermesUrl: 'http://hermes.berrybox.tv',
 };
