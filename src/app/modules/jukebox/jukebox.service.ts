db2457f49a82569c1531a008d1be4b698ca26366
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2019-06-24 11:10:03 +0200

    Delete useless imports

--- src/app/modules/jukebox/jukebox.service.ts
+++ src/app/modules/jukebox/jukebox.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, Observer, Subject, ReplaySubject } from 'rxjs';
+import { BehaviorSubject, Observable, ReplaySubject } from 'rxjs';
 
 import io from 'socket.io-client';
 import * as _ from 'lodash';
