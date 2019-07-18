1a5b0f452f5e68983dfa320ddd7ce1c036a0c77b
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2019-07-18 11:57:30 +0200

    Click on a command to begin using it

--- src/app/modules/jukebox/components/command-list/command-list.component.ts
+++ src/app/modules/jukebox/components/command-list/command-list.component.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Command } from 'app/shared/models/command.model';
 
+/**
+ * List of commands usable in a box.
+ *
+ * @export
+ * @class CommandListComponent
+ * @implements {OnInit}
+ */
 @Component({
     selector: 'app-command-list',
     templateUrl: './command-list.component.html',
     styleUrls: ['./command-list.component.scss']
 })
 export class CommandListComponent implements OnInit {
+    /**
+     * When the user clicks on a command, it is emitted to the panel component that can
+     * then inject it in the chat
+     *
+     * @type {EventEmitter<string>}
+     * @memberof CommandListComponent
+     */
     @Output() clickedCommand: EventEmitter<string> = new EventEmitter<string>();
 
+    /**
+     * List of commands to be displayed
+     *
+     * @type {Array<Command>}
+     * @memberof CommandListComponent
+     */
     commands: Array<Command> = [
         {
             keys: ['add', 'play', 'queue'],
@@ -26,6 +46,16 @@ export class CommandListComponent implements OnInit {
     constructor() { }
 
     ngOnInit() {
+    }
+
+    /**
+     * Triggered when the user clicks on a command. Will emit the key to the parent component
+     *
+     * @param {string} key
+     * @memberof CommandListComponent
+     */
+    useCommand(key: string) {
+        this.clickedCommand.emit(key);
     }
 
 }
