dcac0070f419d2ea7c627576935495e0d59bc1ec
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2019-11-12 11:20:20 +0100

    Add interceptor

--- src/app/modules/admin/pages/admin-panel/admin-panel.component.ts
+++ src/app/modules/admin/pages/admin-panel/admin-panel.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from 'app/shared/models/user.model';
+import { AuthService } from 'app/core/auth/auth.service';
+
 @Component({
     selector: 'app-admin-panel',
     templateUrl: './admin-panel.component.html',
     styleUrls: ['./admin-panel.component.scss']
 })
 export class AdminPanelComponent implements OnInit {
+    public user: User = AuthService.getSession()
+    public currentTab = 'boxes';
 
     constructor() { }
 
