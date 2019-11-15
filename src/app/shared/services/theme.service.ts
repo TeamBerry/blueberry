80e5ed36863969bbbb7a2c1617ca410277ed9106
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2019-11-15 17:30:33 +0100

    Add all variables to light & dark themes

--- src/app/shared/services/theme.service.ts
+++ src/app/shared/services/theme.service.ts
@@ -4,24 +4,34 @@ import { AuthService } from 'app/core/auth/auth.service';
 
 export const darkTheme = {
     'background-main-color': '#0B1A2D',
+    'background-main-hover-color': '#102641',
+    'background-main-active-color': '#1a3e6a',
     'background-main-border-color': '#979797',
     'background-secondary-color': '#0B1A38',
+    'background-secondary-color-alternate': '#0f244d',
     'input-main-color': '#D0D0D0',
     'text-main-color': 'white',
     'background-focus-color': '#192929',
+    'text-focus-color': '#009AEB',
     'text-system-color': '#BBBBBB',
+    'stroke-color': '#BBBBBB',
     'inactive-color': '#CCCCCC'
 }
 
 export const lightTheme = {
     'background-main-color': 'white',
+    'background-main-hover-color': '#f2f2f2',
+    'background-main-active-color': '#d9d9d9',
     'background-main-border-color': '#979797',
     'background-secondary-color': '#0B1A38',
-    'input-main-color': '#D0D0D0',
+    'background-secondary-color-alternate': '#0f244d',
+    'input-main-color': '#d0d0d0',
     'text-main-color': 'black',
-    'background-focus-color': '#192929',
+    'background-focus-color': '#e6e6e6',
+    'text-focus-color': '#009AEB',
     'text-system-color': '#444444',
-    'inactive-color': '#8F8F8F'
+    'stroke-color': '#444444',
+    'inactive-color': '#8f8f8f',
 }
 
 @Injectable({
