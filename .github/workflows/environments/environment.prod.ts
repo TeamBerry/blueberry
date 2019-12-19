d7d9b7143885d1748a566947a9960059a0246e0a
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2019-12-19 15:50:55 +0100

    Revert "echo env file for deploy"
    This reverts commit dbdbb53f0576e6238b2a9e8aa136b17adaff37d2.

---
+++ .github/workflows/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    araza: 'https://araza.berrybox.tv',
+    boquila: 'https://boquila.berrybox.tv',
+    amazonBuckets: 'https://berrybox-user-pictures.s3.eu-west-1.amazonaws.com',
+    profilePictureBuckets: 'profile-pictures'
+};
