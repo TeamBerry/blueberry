0966a0a6f1d95c13fe8c7355904a672217aebf38
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2019-12-19 15:56:25 +0100

    Revert to old env file
    - API Key is restricted to specific app & iP

--- .github/workflows/environments/environment.prod.ts
+++ .github/workflows/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
     araza: 'https://araza.berrybox.tv',
     boquila: 'https://boquila.berrybox.tv',
     amazonBuckets: 'https://berrybox-user-pictures.s3.eu-west-1.amazonaws.com',
-    profilePictureBuckets: 'profile-pictures'
+    profilePictureBuckets: 'profile-pictures',
+    youtubeApiKey: 'AIzaSyB4-S6Lm-1-T1KmGIwcbMw_KDR4T4StxUE'
 };
