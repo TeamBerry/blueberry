8ee309037cf57bf559eab44f689c46f23b9b2723
AngelZatch <angelzatch@gmail.com>
2020-02-10 15:52:54 +0100

    Change bx-button options

--- src/app/shared/components/bx-button/bx-button.component.ts
+++ src/app/shared/components/bx-button/bx-button.component.ts
@@ -1,10 +1,34 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 export interface ButtonOptions {
-    type?: 'play' | 'replay' | 'cancel' | 'skip',
-    tooltip?: string,
+    /**
+     * Type of action of the button. Will determine the icon and the default text
+     *
+     * @type {('play' | 'replay' | 'cancel' | 'skip')}
+     * @memberof ButtonOptions
+     */
+    type: 'play' | 'replay' | 'cancel' | 'skip',
+    /**
+     * Context of the button. Will affect its display
+     *
+     * @type {('primary' | 'secondary' | 'default' | 'warning' | 'default')}
+     * @memberof ButtonOptions
+     */
     context?: 'primary' | 'secondary' | 'default' | 'warning' | 'default',
-    text?: boolean
+    /**
+     * Button text. Will default to the type if not specified
+     *
+     * @type {string}
+     * @memberof ButtonOptions
+     */
+    text?: string,
+    /**
+     * Whether the text must be displayed in the button or in a tooltip
+     *
+     * @type {('button' | 'tooltip')}
+     * @memberof ButtonOptions
+     */
+    textDisplay?: 'button' | 'tooltip'
 }
 
 @Component({
@@ -16,18 +40,24 @@ export class BxButtonComponent implements OnInit {
     @Input() options: ButtonOptions
 
     computedOptions: ButtonOptions = {
-        context: 'default',
-        text: false
+        type: 'play'
     }
+
+    buttonText = ''
 
     constructor() { }
 
     ngOnInit() {
-        Object.keys(this.options).map(
-            (key) => {
-                this.computedOptions[key] = this.options[key]
-            }
-        )
+        this.computedOptions.context = this.options.context || 'default'
+        this.computedOptions.type = this.options.type
+        this.computedOptions.text = this.options.text || this.options.type
+        this.computedOptions.textDisplay = this.options.textDisplay || 'button'
+
+        this.buttonText = this.buildText(this.computedOptions.text)
+    }
+
+    buildText(text: string) {
+        return text[0].toUpperCase() + text.slice(1)
     }
 
 }
