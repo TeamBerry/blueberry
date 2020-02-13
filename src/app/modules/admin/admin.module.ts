b8d5721aae7c4be8afae9e31bef04ca03c4701ba
AngelZatch <angelzatch@gmail.com>
2020-02-13 18:02:30 +0100

    WiP displaying favorites in admin

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -7,13 +7,15 @@ import { AdminPanelComponent } from './pages/admin-panel/admin-panel.component';
 import { BoxesTabComponent } from './components/boxes-tab/boxes-tab.component';
 import { BoxAdminWidgetComponent } from './components/box-admin-widget/box-admin-widget.component';
 import { FavoritesTabComponent } from './components/favorites-tab/favorites-tab.component';
+import { FavoritesAdminWidgetComponent } from './components/favorites-admin-widget/favorites-admin-widget.component';
 
 @NgModule({
     declarations: [
         AdminPanelComponent,
         BoxesTabComponent,
         BoxAdminWidgetComponent,
-        FavoritesTabComponent
+        FavoritesTabComponent,
+        FavoritesAdminWidgetComponent
     ],
     imports: [
         AdminRoutingModule,
--- src/app/modules/admin/components/favorites-admin-widget/favorites-admin-widget.component.ts
+++ src/app/modules/admin/components/favorites-admin-widget/favorites-admin-widget.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Video } from 'app/shared/models/video.model';
 
 @Component({
-  selector: 'app-favorites-admin-widget',
-  templateUrl: './favorites-admin-widget.component.html',
-  styleUrls: ['./favorites-admin-widget.component.scss']
+    selector: 'app-favorites-admin-widget',
+    templateUrl: './favorites-admin-widget.component.html',
+    styleUrls: ['./favorites-admin-widget.component.scss']
 })
 export class FavoritesAdminWidgetComponent implements OnInit {
 
-  constructor() { }
+    @Input() video: Video;
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
