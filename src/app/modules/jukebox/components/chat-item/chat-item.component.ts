81b59faa9295948115a90b577aab4ba4ff04d0b5
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2020-02-26 12:54:55 +0100

    Style feedback messages

---
+++ src/app/modules/jukebox/components/chat-item/chat-item.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Message } from 'app/shared/models/message.model';
+import { FeedbackMessage } from '@teamberry/muscadine';
+
+@Component({
+    selector: 'app-chat-item',
+    templateUrl: './chat-item.component.html',
+    styleUrls: ['./chat-item.component.scss']
+})
+export class ChatItemComponent implements OnInit {
+    @Input() message: Message | FeedbackMessage;
+
+    typeStyle: string;
+
+    constructor() { }
+
+    ngOnInit() {
+        this.typeStyle = 'feedbackType' in this.message ? `system-message-${this.message.feedbackType}` : null;
+        console.log(this.typeStyle);
+    }
+
+}
