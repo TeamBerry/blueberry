d04276865d129bc20d5d3f6494bef5f4691dcce8
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2020-02-26 14:08:45 +0100

    Compute author separately from message

--- src/app/modules/jukebox/components/chat-item/chat-item.component.ts
+++ src/app/modules/jukebox/components/chat-item/chat-item.component.ts
@@ -10,12 +10,15 @@ export class ChatItemComponent implements OnInit {
     @Input() message: Message | FeedbackMessage;
 
     typeStyle: string;
+    author: string;
 
     constructor() { }
 
     ngOnInit() {
+        this.author = this.message.author
+            ? (typeof this.message.author === 'object' ? this.message.author.name : this.message.author)
+            : null;
         this.typeStyle = 'feedbackType' in this.message ? `system-message-${this.message.feedbackType}` : null;
-        console.log(this.typeStyle);
     }
 
 }
