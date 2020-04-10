03f894c40a6a956306654e652b20651fb10c8a67
AngelZatch <angelzatch@gmail.com>
2020-04-10 15:28:27 +0200

    UI for preselection
    - Fixes svg in buttons

--- src/app/shared/components/bx-button/bx-button.component.ts
+++ src/app/shared/components/bx-button/bx-button.component.ts
@@ -7,7 +7,7 @@ export interface ButtonOptions {
      * @type {('play' | 'replay' | 'cancel' | 'skip')}
      * @memberof ButtonOptions
      */
-    type: 'play' | 'replay' | 'cancel' | 'skip' | 'addToLibrary',
+    type: 'play' | 'replay' | 'cancel' | 'skip' | 'addToLibrary' | 'preselect',
     /**
      * Context of the button. Will affect its display
      *
