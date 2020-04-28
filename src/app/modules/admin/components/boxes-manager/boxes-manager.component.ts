ffe71115ec128762ebb59ede6cdda3c979792a00
AngelZatch <angelzatch@gmail.com>
2020-04-28 11:55:34 +0200

    feat: life clock

--- src/app/modules/admin/components/boxes-manager/boxes-manager.component.ts
+++ src/app/modules/admin/components/boxes-manager/boxes-manager.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
 import * as _ from 'lodash';
+import * as moment from 'moment';
 
 import { UserService } from 'app/shared/services/user.service';
 import { AuthService } from 'app/core/auth/auth.service';
@@ -21,9 +22,16 @@ import { AuthSubject } from 'app/shared/models/session.model';
 export class BoxesManagerComponent implements OnInit {
     user: AuthSubject = AuthService.getAuthSubject();
 
-    public boxes: Observable<Array<Box>>;
+    public boxes: Array<Box>;
+    selectedBox: Box = null;
+
+    lifetime = null;
+    clockInterval;
+
+    users: number;
 
     constructor(
+        private boxService: BoxService,
         private modalService: NgbModal,
         private userService: UserService,
     ) {
@@ -35,11 +43,73 @@ export class BoxesManagerComponent implements OnInit {
     }
 
     loadBoxes() {
-        this.boxes = this.userService.boxes(this.user);
+        this.userService.boxes(this.user).subscribe(
+            (boxes: Array<Box>) => {
+                this.boxes = boxes;
+
+                if (!this.selectedBox && boxes.length > 0) {
+                    this.selectBox(boxes[0]._id);
+                }
+            }
+        );
     }
 
     openCreateModal() {
         const modalRef = this.modalService.open(BoxFormComponent);
         modalRef.componentInstance.title = 'Create a box';
+    }
+
+    selectBox(boxId: string) {
+        if (!this.selectedBox || this.selectedBox._id !== boxId) {
+            clearInterval(this.clockInterval);
+            this.selectedBox = this.boxes.find((box: Box) => box._id === boxId)
+            this.setupClock(this.selectedBox.createdAt)
+            this.boxService.users(boxId).subscribe(
+                (users) => {
+                    this.users = users.length;
+                }
+            )
+        }
+    }
+
+    setupClock(startTime: Date) {
+        console.log(startTime);
+        let life = Math.floor((Date.now() - Date.parse(startTime.toString())) / 1000);
+        this.displayClock(life);
+
+        this.clockInterval = setInterval(() => {
+            life++;
+            this.displayClock(life);
+        }, 1000);
+    }
+
+    displayClock(time: number) {
+        let computedDuration = `PT`
+
+        const years = Math.floor(time / 31557600);
+        if (years > 0) {
+            computedDuration += `${years}Y`
+        }
+
+        const days = Math.floor((time - (years * 31557600)) / 86400);
+        if (days > 0 || years !== 0) {
+            computedDuration += `${days}D`
+        }
+
+        const hours = Math.floor((time - (years * 31557600) - (days * 86400)) / 3600);
+        if (hours > 0 || days !== 0 || years !== 0) {
+            computedDuration += `${hours}H`
+        }
+
+        const mins = Math.floor((time - (years * 31557600) - (days * 86400) - (hours * 3600)) / 60);
+        if (mins > 0 || hours !== 0 || days !== 0 || years !== 0) {
+            computedDuration += `${mins}M`
+        }
+
+        const seconds = time - (years * 31557600) - (days * 86400) - (hours * 3600) - (mins * 60);
+        computedDuration += `${seconds}S`
+
+        this.lifetime = computedDuration;
+
     }
 }
