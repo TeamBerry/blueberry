6ebe2cc29e4b47650ee9d99fb7aba875fdc34801
AngelZatch <angelzatch@gmail.com>
2020-08-28 15:21:31 +0200

    feat: privacy policy page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './shared/components/home/home.component';
 import { PasswordResetComponent } from './pages/password-reset/password-reset.component';
+import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
     { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) },
     // { path: 'playlists', loadChildren: () => import('./modules/user-playlists/user-playlists.module').then(m => m.UserPlaylistsModule) },
-    { path: 'password/reset', component: PasswordResetComponent }
+    { path: 'password/reset', component: PasswordResetComponent },
+    { path: 'privacy', component: PrivacyPolicyComponent }
 ];
 
 @NgModule({
