1c64768f5418e0170c1eb07c0d10949151741b78
AngelZatch <angelzatch@gmail.com>
2020-11-30 12:57:30 +0100

    chore: settings is now a button

--- src/app/shared/components/bx-button/bx-button.component.ts
+++ src/app/shared/components/bx-button/bx-button.component.ts
@@ -7,7 +7,7 @@ export interface ButtonOptions {
      * @type {('play' | 'replay' | 'cancel' | 'skip')}
      * @memberof ButtonOptions
      */
-    type: 'play' | 'replay' | 'cancel' | 'skip' | 'addToLibrary' | 'forceNext' | 'forcePlay' | 'inviteUser',
+    type: 'play' | 'replay' | 'cancel' | 'skip' | 'addToLibrary' | 'forceNext' | 'forcePlay' | 'inviteUser' | 'settings',
     /**
      * Context of the button. Will affect its display
      *
