4c9c4403c7827c1a6b56c462fb1e7cc882c17af8
AngelZatch <angelzatch@gmail.com>
2020-12-29 15:12:02 +0100

    fix: prod env file

--- .github/workflows/environments/environment.prod.ts
+++ .github/workflows/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     araza: 'https://araza.berrybox.tv',
     boquila: 'https://boquila.berrybox.tv',
-    amazonBuckets: 'https://berrybox-user-pictures.s3.eu-west-1.amazonaws.com',
+    userContentBucket: 'https://berrybox-user-pictures.s3.eu-west-1.amazonaws.com',
     profilePictureBuckets: 'profile-pictures',
     adminId: '5dcd89d6fc9a6c5b3758a0ae'
 };
