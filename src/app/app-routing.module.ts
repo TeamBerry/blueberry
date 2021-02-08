4287d942d1d96aaa086965f5c928dc2064099ae0
AngelZatch <angelzatch@gmail.com>
2021-02-08 10:46:13 +0100

    feat: invite parse page
    closes #285

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './shared/components/home/home.component';
 import { PasswordResetComponent } from './pages/password-reset/password-reset.component';
 import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
+import { InviteComponent } from './pages/invite/invite.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
+    { path: 'invite/:link', component: InviteComponent },
+    { path: 'i/:link', component: InviteComponent },
     { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) },
     { path: 'collection', loadChildren: () => import('./modules/collection/collection.module').then(m => m.CollectionModule) },
     { path: 'password/reset', component: PasswordResetComponent },
